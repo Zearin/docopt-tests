2cc6f9138c26151f2030da93be4926d2ba599d55
Zearin <zearin@gonk.net>
2014-09-07 09:57:40 -0400

    Good enough for testing (for now).

--- docopt_tests.py
+++ docopt_tests.py
@@ -3,13 +3,16 @@
 # Standard Lib
 from    __future__  import (print_function, with_statement, unicode_literals)
 import  atexit
+import  contextlib
 import  json
 import  logging
 import  re
+import  textwrap
 
 # Helpers
+from    colorama import (Fore, Style)
 import  lib, lib.json
-from path   import path
+from    path   import path
 
 # Testing
 from preggy import expect
@@ -26,43 +29,40 @@ BUILDDIR    = 'build'
 INDENT      = '  '
 TESTFILES   = path(BUILDDIR).files('*.json')
 
-log = logging.getLogger('docopt_tests')
-log.setLevel(logging.WARN)
+log         = None
 
 #--------------------------------------------------------------------------------
-    
-def _handle_failed_test( kw ):
-    file, scenario, usage = kw['file'], kw['scenario_data'][1], kw['usage_data'][1]
-    
-    if file not in FAILURES:
-        FAILURES[file] = {}
-    # if feature not in FAILURES[file]:
-    #     FAILURES[file][feature] = {}
-    if scenario not in FAILURES[file]:
-        FAILURES[file][scenario] = {}
-    if usage not in FAILURES[file][scenario]:
-        FAILURES[file][scenario][usage] = []
-    
-    FAILURES[file][scenario][usage].append(kw)
-    
-    
-    lookup_result = lib.src_map_peek(path(file).basename(), kw['usage_data'][0], BUILDDIR)
-    lookup_output = lookup_result.std_out.splitlines()
-    print(lookup_output[-2:])
-    
-    print('Failed test #{test_index} for usage:{usage_data[1]}'.format( **kw ) )
+
+def _handle_failed_test( ctx ):
+    src_file, src_line = ctx['cson_err_location']
+    src_map_peek = ctx['src_map_peek']
+    feature, scenario, usage = ctx['feature_name'], ctx['scenario_name'], ctx['usage']
+
+    if src_file not in FAILURES:
+        FAILURES[src_file] = set()
+    if src_line not in FAILURES[src_file]:
+        FAILURES[src_file].add((src_map_peek, ctx['error']))
+
+    # if feature not in FAILURES[src_file]:
+    #     FAILURES[src_file][feature] = {}
+    # if scenario not in FAILURES[src_file]:
+    #     FAILURES[src_file][scenario] = {}
+    # if usage not in FAILURES[src_file][scenario]:
+    #     FAILURES[src_file][scenario][usage] = set()
+    #
+    # FAILURES[src_file][scenario][usage].add(src_line)
 
 def _show_failed_usages():
     from pprint import pprint
     from colorama import (Fore, Back, Style)
-    
+
     FAILCOUNT=0
-    
+
     for scenario in FAILURES.keys():
         print('{Style.DIM}{banner}{Style.RESET_ALL}'.format(Style=Style, banner=('-' * 80)))
         print(scenario)
         print()
-        
+
         for doc, failures in FAILURES[scenario].items():
             print("  {Fore.WHITE}{Style.DIM}{banner}{Style.RESET_ALL}{Fore.RESET}".format(Fore=Fore, Style=Style, banner=('-' * 80) ))
             print("  {Fore.WHITE}{doc}{Fore.RESET}".format(Fore=Fore, doc=doc))
@@ -70,25 +70,25 @@ def _show_failed_usages():
                 print("    {Fore.RED}{0}{Fore.RESET}".format(fail,Fore=Fore))
                 FAILCOUNT += 1
             print()
-        
+
         print()
         print()
-        
+
 
     print("\n{Fore.WHITE}Total Failures: {0}{Fore.RESET}".format(FAILCOUNT, Fore=Fore))
 
-    
+
 #atexit.register(_show_failed_usages)
 
 def _scrape_srcmap_stdout(srcmap_out):
     '''Scrapes the the output of :func:`lookup_srcmap`, and returns a tuple ``(file, line)``.
     '''
-    
+
     FLAGS     = re.IGNORECASE|re.MULTILINE|re.IGNORECASE|re.UNICODE#|re.DEBUG
     SRCMAP_RE = re.compile(
         r'''^file:\s(.+)\nline:\s(\d+)\s''',
         flags=FLAGS)
-    
+
     # relevant data is in the last 2 lines of output
     srcmap_out = '\n'.join( srcmap_out.splitlines()[-2:] )
     match      = SRCMAP_RE.match(srcmap_out)
@@ -97,64 +97,74 @@ def _scrape_srcmap_stdout(srcmap_out):
 
 #-------------------------------------------------------------------------------
 
-import contextlib
-
 @contextlib.contextmanager
-def scenario_context(ctx):
-    #error_in_scenario = False
-    log.info('SCENARIO: %s', ctx['scenario_name'])
+def feature_context(ctx, **f_ctx):
+    log.info('FEATURE: %s', f_ctx['feature_name'])
     
-    ctx['scenario_line'] = None
-    for line, value in ctx['indeces']['scenarios']:
-        if ctx['scenario_name'] in value:
-            ctx['scenario_line'] = line
+    ctx.update(f_ctx)
     
     yield ctx
     
-    for i in ('scenario_name','scenario','scenario_line'):
-        del ctx[i]
-        
+    for key in f_ctx.keys():
+        del ctx[key]
+
+@contextlib.contextmanager
+def scenario_context(ctx, **s_ctx):
+    log.info('SCENARIO: %s', s_ctx['scenario_name'])
+
+    s_ctx['scenario_line'] = None
+    for line, value in ctx['indeces']['scenarios']:
+        if s_ctx['scenario_name'] in value:
+            s_ctx['scenario_line'] = line
+
+    ctx.update(s_ctx)
+
+    yield ctx
+
+    for key in s_ctx.keys():
+        del ctx[key]
 
 
 @contextlib.contextmanager
-def usage_context(ctx):
-    #error_in_usage = False
-    log.info('%s', ctx['usage'])
-    
-    ctx['usage_line'] = None
+def usage_context(ctx, **u_ctx):
+    log.info('%s', u_ctx['usage'])
+
+    u_ctx['usage_line'] = None
     for line, value in ctx['indeces']['usages']:
         if line < ctx['scenario_line']:  # Skip earlier lines
             continue
-        if ctx['usage'] in value.replace(r'\n', '\n'):  #  Usages often span multiple lines
-            ctx['usage_line'] = line
-    
+        if u_ctx['usage'] in value.replace(r'\n', '\n'):  #  Usages often span multiple lines
+            u_ctx['usage_line'] = line
+
     try:
-        ctx['next_usage_line'] = min([line for line, value in ctx['indeces']['usages'] if line > ctx['usage_line']])
+        u_ctx['next_usage_line'] = min([line for line, value in ctx['indeces']['usages'] if line > u_ctx['usage_line']])
     except ValueError:
-        ctx['next_usage_line'] = 0
-    
+        u_ctx['next_usage_line'] = 0
+
+    ctx.update(u_ctx)
+
     yield ctx
-    
-    for i in ('usage_index','usage','tests','usage_line','next_usage_line'):
-        del ctx[i]
+
+    for key in u_ctx:
+        del ctx[key]
 
 @contextlib.contextmanager
-def test_context(ctx):
+def test_context(ctx, **t_ctx):
     # TESTS
-    ctx['test_line'] = None
-    
+    t_ctx['test_line'] = None
+
     for line, value in [
-        (line, value) for (line, value) in ctx['indeces']['tests'] 
-        if ctx['next_usage_line'] > line > ctx['usage_line'] 
+        (line, value) for (line, value) in ctx['indeces']['tests']
+        if ctx['next_usage_line'] > line > ctx['usage_line']
     ]:
-        if ctx['test']['input'] in value:
-            ctx['test_line'] = line
-    
-    log.info('ARGV: %s', ctx['test']['input'])
-    
+        if t_ctx['test']['input'] in value:
+            t_ctx['test_line'] = line
+
+    log.info('ARGV: %s', t_ctx['test']['input'])
+
     # run the test
     try:
-        argv, expected = ctx['test']['input'], ctx['test']['expected']
+        argv, expected = t_ctx['test']['input'], t_ctx['test']['expected']
         if expected == 'user-error':
             try:
                 expect( docopt(ctx['usage'], argv, help=False) ).to_be_an_error()
@@ -165,33 +175,54 @@ def test_context(ctx):
                 expect( docopt(ctx['usage'], argv, help=False) ).to_equal(expected)
             except (DocoptExit,SystemExit): # prevent docopt from stopping testing
                 pass
-    
+
     # test failed
     except AssertionError as err:
-        _error_source = lib.src_map_peek(
-            ctx['testfile'].basename(), 
-            ctx['test_line'],
-            cwd=BUILDDIR)
-        _error_ctx = _scrape_srcmap_stdout(_error_source.std_out)
-        if _error_ctx[0].startswith('../src'):
-            _error_ctx[0] = _error_ctx[0][3:]
-        ctx['error'] = _error_ctx
+        ctx['error'] = err
         
+        _src_map_peek = lib.src_map_peek(
+            ctx['testfile'].basename(),
+            t_ctx['test_line'],
+            cwd=BUILDDIR)
+         
+        # fix results (they're relative to BUILDDIR)
+        _src_map_peek.std_out =  _src_map_peek.std_out.replace('../src', 'src')
+        
+        ctx['src_map_peek'] = _src_map_peek.std_out
+        ctx['cson_err_location'] = _scrape_srcmap_stdout(_src_map_peek.std_out)
+        
+        
+
     finally:
         yield ctx
-        #_handle_failed_test(fail_data)
-        for i in ('test_index','test','test_line','error'):
-            try:                del ctx[i]
-            except KeyError:    pass
+
+        for key in t_ctx.keys():
+            try:
+                del ctx[key]
+            except KeyError:
+                pass
 
 
 # JSON File
 for testfile in TESTFILES:
     
+    # Grab a file-specific logger 
+    log = logging.getLogger(testfile.namebase)
+    log.setLevel(logging.WARN)
+    
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.WARN)
+    
+    formatter = logging.Formatter('%(name)s:%(message)s')
+    
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    
+    # Begin
     log.debug('='*80)
     log.debug('%s', testfile.relpath())
     log.debug('='*80)
-    
+
     # First, index the important keys
     #
     # (necessary because there's no way to tell the result of `json.loads()`:
@@ -199,85 +230,89 @@ for testfile in TESTFILES:
     #
     lines     = testfile.lines(encoding='UTF-8')
     indeces   = lib.json.get_line_indeces(lines, indent=INDENT)
-    
+
     log.debug('\t%d Features',  len(indeces['features']))
     log.debug('\t%d Scenarios', len(indeces['scenarios']))
     log.debug('\t%d Usages',    len(indeces['usages']))
-    
+
     # Deserialize JSON
     contents  = testfile.text(encoding='UTF-8')
     json_data = json.loads(
-        contents, 
-        object_hook=lib.json.decode.decode_dict, 
-        encoding   ='UTF-8', 
+        contents,
+        object_hook=lib.json.decode.decode_dict,
+        encoding   ='UTF-8',
         strict     =False
         )
-    
+
     ctx = {
         'testfile': testfile,
         'indeces':  indeces
     }
-    
-    
+
+
     # FEATURES
     for feature_name, feature in json_data.items():
-        #error_in_feature = False
-        log.info('FEATURE: %s', feature_name)
         
-        ctx.update({
-            'feature_name': feature_name,
-            'feature':      feature
-        })
-        
-        
-        # SCENARIOS
-        for scenario_name, scenario in ctx['feature'].items():
-            if scenario_name == '__desc':  # skip descriptions
-                continue
+        with feature_context(ctx, feature_name=feature_name, feature=feature) as ctx:
             
-            ctx.update({
-                'scenario_name': scenario_name,
-                'scenario':      scenario 
-            })
-            
-            
-            with scenario_context(ctx) as ctx:
-                
-                
-                # USAGE
-                for usage_index, (usage, tests) in enumerate(ctx['scenario'].items()):
-                    ctx.update({
-                        'usage_index': usage_index,
-                        'usage':       usage,
-                        'tests':       tests
-                    })
-                    
-                    
-                    with usage_context(ctx) as ctx:
-                        for test_index, test in enumerate(ctx['tests']):
-                            ctx.update({
-                                'test_index': test_index,
-                                'test':       test
-                            })
+            # SCENARIOS
+            for scenario_name, scenario in ctx['feature'].items():
+                if scenario_name == '__desc':  # skip descriptions
+                    continue
+
+                with scenario_context(ctx, scenario_name=scenario_name, scenario=scenario) as ctx:
+
+                    # USAGES
+                    for usage_index, (usage, tests) in enumerate(ctx['scenario'].items()):
+
+                        with usage_context(ctx, usage_index=usage_index, usage=usage, tests=tests) as ctx:
                             
-                            with test_context(ctx) as ctx:
-                                if 'error' in ctx:
-                                    src_file, src_line = ctx['error']
-                                    
-                                    if src_file not in FAILURES:
-                                        FAILURES[src_file] = []
-                                    FAILURES[src_file].append(src_line)
-                            
-        
+                            # TESTS
+                            for test_index, test in enumerate(ctx['tests']):
+
+                                with test_context(ctx, test_index=test_index, test=test) as ctx:
+                                    if 'error' in ctx:
+                                        _handle_failed_test(ctx)
+
+
+#  Report results when testing is complete
+if len(FAILURES):
+    FAILCOUNT = 0
     
-for src_file, line_numbers in FAILURES.iteritems():
-    print(src_file)
-    for i in sorted(line_numbers):
-        print('\t{0}'.format(i))
+    preggywrap = textwrap.TextWrapper(
+        break_long_words    = False,
+        break_on_hyphens    = False,
+        initial_indent      = INDENT*2,
+        subsequent_indent   = INDENT*2,
+        drop_whitespace     = False,
+        replace_whitespace  = False,
+        expand_tabs         = False,
+        width               = 120
+        )
+    
+    srcmapwrap = textwrap.TextWrapper(
+        break_long_words    = False,
+        break_on_hyphens    = False,
+        initial_indent      = INDENT*4,
+        subsequent_indent   = INDENT*4,
+        drop_whitespace     = False,
+        replace_whitespace  = False,
+        expand_tabs         = False,
+        width               = 100
+        )
+    
+    for key in FAILURES.keys():
+        print(Style.BRIGHT, key, Style.RESET_ALL)
+        
+        for failure in FAILURES[key]:
+            FAILCOUNT += 1
+            src_map_peek, err = failure
+            
+            print('\n\n', Fore.YELLOW, preggywrap.fill(str(err)), Fore.RESET, sep=None, end='\n\n')
+            for line in src_map_peek.splitlines():
+                print(''.join(srcmapwrap.wrap(line)))
+        
+        print('\n')
+        
 
-
-# # Report the test results
-# if len(FAILURES) == 0:
-#     log.info('No tests were failed!')
-# else:
-#     log.warn('{0} failed tests'.format(len(FAILURES)))
+    print('TOTAL FAILURES: %s' % FAILCOUNT)
